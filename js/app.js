30b907565bc38f9e7f321c6b95a3c5a813f320f0
Mohammed Nurul Abedin Ashraf <nurulabedinashraf@gmail.com>
2023-06-14 14:50:55 -0500

    added functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
-function generatePin() {
-    console.log('Generating');
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if (pinString.length == 4) {
+        return pin;
+    }
+
+    else {
+        return getPin();
+    }
 }
+
+
+function generatePin() {
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+}
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+
+    }
+    else {
+
+        calcInput.value += number;
+    }
+})
